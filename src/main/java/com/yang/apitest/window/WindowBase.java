0dc72c0139693d1da73be8d6e73fbd7072096b00
YangFighting <yangzhang_y@163.com>
2022-05-12 18:12:53 +0800

    【M】 计数窗口

--- src/main/java/com/yang/apitest/window/WindowBase.java
+++ src/main/java/com/yang/apitest/window/WindowBase.java
@@ -18,16 +18,24 @@ public class WindowBase {
         DataStream<Topic001> dataStream = InputDataStreamUtil.getTopoc001FromStringDataStream(inputDataStream);
         KeyedStream<Topic001, Integer> keyByDataStream = dataStream.keyBy(Topic001::getId);
 
-        // 滚动窗口
+        // TimeWindow 滚动窗口
         DataStream<Topic001> minByScrollWindowStream = keyByDataStream.window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
                 .minBy("num");
-        minByScrollWindowStream.print("minByScrollWindowStream");
+        minByScrollWindowStream.print(" Scroll ");
 
-        // 滑动窗口
+        // TimeWindow 滑动窗口
         DataStream<Topic001> minBySlidingWindowStream = keyByDataStream
                 .window(TumblingProcessingTimeWindows.of(Time.seconds(15), Time.seconds(5)))
                 .minBy("num");
-        minBySlidingWindowStream.print("minBySlidingWindowStream");
+        minBySlidingWindowStream.print(" Sliding ");
+
+        // CountWindow  滚动窗口
+        DataStream<Topic001> minByScrollCount = keyByDataStream.countWindow(5).minBy("num");
+        minByScrollCount.print("Scroll countWindow ");
+
+        // CountWindow  滑动窗口
+        DataStream<Topic001> minBySlidingCount = keyByDataStream.countWindow(10 ,2).minBy("num");
+        minBySlidingCount.print("Sliding countWindow ");
 
         InputDataStreamUtil.execute("WindowBase");
     }
