7d1511e087da34c35df10218533cc9f7e2c65ae9
YangFighting <yangzhang_y@163.com>
2022-05-12 10:57:12 +0800

    【M】 时间窗口-滑动窗口

--- src/main/java/com/yang/apitest/window/WindowBase.java
+++ src/main/java/com/yang/apitest/window/WindowBase.java
@@ -3,7 +3,7 @@ package com.yang.apitest.window;
 import com.yang.apitest.pojo.Topic001;
 import com.yang.utils.InputDataStreamUtil;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 
@@ -16,14 +16,18 @@ public class WindowBase {
     public static void main(String[] args) throws Exception {
         DataStream<String> inputDataStream = InputDataStreamUtil.getDataStreamFromSocket("localhost", 7777);
         DataStream<Topic001> dataStream = InputDataStreamUtil.getTopoc001FromStringDataStream(inputDataStream);
+        KeyedStream<Topic001, Integer> keyByDataStream = dataStream.keyBy(Topic001::getId);
 
         // 滚动窗口
-        SingleOutputStreamOperator<Topic001> minByWindowStream = dataStream.keyBy(Topic001::getId)
-                .window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
+        DataStream<Topic001> minByScrollWindowStream = keyByDataStream.window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
                 .minBy("num");
+        minByScrollWindowStream.print("minByScrollWindowStream");
 
-        minByWindowStream.print("minByWindowStream");
-
+        // 滑动窗口
+        DataStream<Topic001> minBySlidingWindowStream = keyByDataStream
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(15), Time.seconds(5)))
+                .minBy("num");
+        minBySlidingWindowStream.print("minBySlidingWindowStream");
 
         InputDataStreamUtil.execute("WindowBase");
     }
